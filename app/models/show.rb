8e458dbc912e7047478a785a3a473571b9a7e408
blackwell <blackwellsmithiv@gmail.com>
2019-04-03 13:11:40 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,37 +1,37 @@
 class Show < ActiveRecord::Base
 
-def highest_rating
-  show = Show.maximum(:rating)
-  show.rating
-end
+  def highest_rating
+    show = Show.maximum(:rating)
+    show.rating
+  end
 
-def most_popular_show
-  show = Show.maximum(:rating)
-  show.name
-end
+  def most_popular_show
+    show = Show.maximum(:rating)
+    show.name
+  end
 
-def lowest_rating
-  sad_show = Show.minimum(:rating)
-  sad_show.rating
-end
+  def lowest_rating
+    sad_show = Show.minimum(:rating)
+    sad_show.rating
+  end
 
-def least_popular_show
-  sad_show = Show.minimum(:rating)
-  sad_show.name
-end
+  def least_popular_show
+    sad_show = Show.minimum(:rating)
+    sad_show.name
+  end
 
-def ratings_sum
-  Show.sum(:rating)
-end
+  def ratings_sum
+    Show.sum(:rating)
+  end
 
-def popular_shows
-  Show.where(:rating < 5).map {|s| s.name}
+  def popular_shows
+    Show.where(:rating < 5).map {|s| s.name}
   end
 
 
-def show_by_alphabetical_order
-  Show.order(:name).map { |n| n.name }
+  def show_by_alphabetical_order
+    Show.order(:name).map { |n| n.name }
 
-end
+  end
 end
 
